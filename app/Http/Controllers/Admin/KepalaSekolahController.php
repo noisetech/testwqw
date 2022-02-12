09b156233a9e3a16014fd2e60d34161a1c8c8df2
noisetech <box92143@gmail.com>
2022-02-12 07:49:11 +0700

    tambah

--- app/Http/Controllers/Admin/KepalaSekolahController.php
+++ app/Http/Controllers/Admin/KepalaSekolahController.php
@@ -3,9 +3,12 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\KepalaSekolah;
 use Illuminate\Http\Request;
 
 class KepalaSekolahController extends Controller
 {
-    //
+    public function index(){
+        $items = KepalaSekolah::all();
+    }
 }
